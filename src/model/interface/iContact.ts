1d374a4d58be46a17244dbd963f2ebd5693a6e05
José Ruan <jrpaivadev@gmail.com>
2023-11-02 20:41:31 -0300

    Update iContact.ts

--- src/model/interface/iContact.ts
+++ src/model/interface/iContact.ts
@@ -2,7 +2,7 @@ export interface Icontact {
     getId(): number
     getEmail(): string
     getPhoneNumber(): string
-    setEmail(value: string): void
-    setPhoneNumber(value: string): void
+    setEmail(value: string): string
+    setPhoneNumber(value: string): string
     toString(): string
 } 
