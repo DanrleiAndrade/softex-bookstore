cf4768065aa6c27a695d5bfa613fc8a78b3c0174
Vinicius Oliveira <vinicius.dsro@hotmail.com>
2023-11-07 08:32:09 -0300

    Delentando

--- src/model/interface/iPerson.ts
+++
@@ -1,9 +0,0 @@
-export interface iPerson{
-    getId(): number
-    getName(): string
-    getBirthday(): string
-    getCpf(): string
-    setName(name: string): string
-    setBirthday(birthday: string): string
-    setCpf(cpf: string): string
-}
